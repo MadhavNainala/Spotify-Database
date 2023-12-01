f9b8ab886bef5eb7747fb0da43e3b8de26339300
Srikanth Chilaka <113320805+SrikanthChilaka@users.noreply.github.com>
2023-12-01 04:36:32 -0500

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -7,6 +7,7 @@ AND EXTRACT(YEAR FROM h.History_Date) = EXTRACT(YEAR FROM CURRENT_DATE)
 GROUP BY s.Song_Id, s.Song_Name
 ORDER BY Listen_Count DESC
 FETCH FIRST 5 ROW ONLY;
+/
 
 CREATE OR REPLACE VIEW Most_Downloaded_Song_And_Album AS
 SELECT s.Song_Name, a.Album_Name
@@ -16,6 +17,7 @@ JOIN Downloaded_Songs ds ON s.Song_Id = ds.Song_Id
 GROUP BY s.Song_Name, a.Album_Name
 ORDER BY COUNT(*) DESC
 FETCH FIRST 5 ROW ONLY;
+/
 
 CREATE OR REPLACE VIEW Most_Used_Promocodes AS
 SELECT p.Promocode_Name, COUNT(*) AS Usage_Count
@@ -24,6 +26,7 @@ JOIN Transaction t ON p.Promocode_Id = t.Promocode_Id
 GROUP BY p.Promocode_Name
 ORDER BY Usage_Count DESC
 FETCH FIRST 5 ROW ONLY;
+/
 
 CREATE OR REPLACE VIEW Most_Listened_Genre_By_Customer AS
 SELECT s.Genre, COUNT(*) AS Listen_Count
@@ -31,9 +34,106 @@ FROM History h
 JOIN Songs s ON h.Song_Id = s.Song_Id
 GROUP BY s.Genre
 ORDER BY Listen_Count DESC;
+/
 
 CREATE OR REPLACE VIEW Most_Used_Payment_Type AS
 SELECT pm.Payment_Type, COUNT(*) AS Usage_Count
 FROM Payment_Method pm
 GROUP BY pm.Payment_Type
 ORDER BY Usage_Count DESC;
+/
+
+CREATE OR REPLACE VIEW Avg_Transaction_Price_Per_Customer AS
+SELECT Customer_Id, AVG(Price) AS Avg_Price
+FROM Transaction
+GROUP BY Customer_Id;
+/
+
+CREATE OR REPLACE VIEW Song_Count_By_Genre AS
+SELECT Genre, COUNT(*) AS Song_Count
+FROM Songs
+GROUP BY Genre;
+/
+
+CREATE OR REPLACE VIEW Revenue_Per_Transaction AS
+SELECT Transaction_Id, SUM(Price) AS Total_Revenue, AVG(Price) AS Avg_Price
+FROM Transaction
+GROUP BY Transaction_Id;
+/
+
+CREATE OR REPLACE VIEW Most_Active_Customers AS
+SELECT Customer_Id, COUNT(*) AS Transaction_Count
+FROM Transaction
+GROUP BY Customer_Id
+ORDER BY Transaction_Count DESC
+FETCH FIRST 5 ROW ONLY;
+/
+
+CREATE OR REPLACE VIEW Revenue_By_Payment_Type AS
+SELECT pm.Payment_Type, SUM(t.Price) AS Total_Revenue
+FROM Payment_Method pm
+JOIN Transaction t ON pm.Customer_Id = t.Customer_Id
+GROUP BY pm.Payment_Type;
+/
+
+CREATE OR REPLACE VIEW Transactions_Per_Customer_Per_Month AS
+SELECT
+    Customer_Id,
+    TO_CHAR(Transaction_Time, 'YYYY-MM') AS Month,
+    COUNT(*) AS Transaction_Count
+FROM Transaction
+GROUP BY Customer_Id, TO_CHAR(Transaction_Time, 'YYYY-MM');
+/
+
+CREATE OR REPLACE VIEW Avg_Promocode_Discount AS
+SELECT
+    Promocode_Id,
+    AVG(Discount) AS Avg_Discount
+FROM Promocodes
+GROUP BY Promocode_Id;
+/
+
+CREATE OR REPLACE VIEW Most_Popular_Genre_Active_Subscribers AS
+SELECT
+    s.Genre,
+    COUNT(*) AS Subscriber_Count
+FROM
+    History h
+JOIN
+    Songs s ON h.Song_Id = s.Song_Id
+JOIN
+    Customers c ON h.Customer_Id = c.Customer_Id
+WHERE
+    c.Subscription = 'Y'
+GROUP BY
+    s.Genre
+ORDER BY
+    Subscriber_Count DESC;
+/
+
+CREATE OR REPLACE VIEW Total_Revenue_Per_Customer AS
+SELECT
+    Customer_Id,
+    SUM(Price) AS Total_Revenue
+FROM
+    Transaction
+GROUP BY
+    Customer_Id;
+/
+
+CREATE OR REPLACE VIEW Avg_Downloads_Per_Song AS
+SELECT
+    Song_Id,
+    AVG(Download_Count) AS Avg_Downloads
+FROM (
+    SELECT
+        Song_Id,
+        COUNT(*) AS Download_Count
+    FROM
+        Downloaded_Songs
+    GROUP BY
+        Song_Id
+) ds
+GROUP BY
+    Song_Id;
+/
