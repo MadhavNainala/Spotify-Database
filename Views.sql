ceffa858a407a7bd0e05ee756847374604219758
Srikanth Chilaka <113320805+SrikanthChilaka@users.noreply.github.com>
2023-12-01 19:21:17 -0500

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,3 +1,4 @@
+--views 
 CREATE OR REPLACE VIEW Most_Listened_Song_Of_The_Month AS
 SELECT s.Song_Id, s.Song_Name, COUNT(*) AS Listen_Count
 FROM Songs s
@@ -137,3 +138,89 @@ FROM (
 GROUP BY
     Song_Id;
 /
+
+CREATE OR REPLACE VIEW Customers_View AS
+SELECT * FROM Customers;
+
+CREATE OR REPLACE VIEW Promocodes_View AS
+SELECT * FROM Promocodes;
+
+CREATE OR REPLACE VIEW Payment_Method_View AS
+SELECT * FROM Payment_Method;
+
+CREATE OR REPLACE VIEW Bank_Account_View AS
+SELECT * FROM Bank_Account;
+
+CREATE OR REPLACE VIEW Card_View AS
+SELECT * FROM Card;
+
+CREATE OR REPLACE VIEW Transaction_View AS
+SELECT * FROM Transaction;
+
+CREATE OR REPLACE VIEW Album_View AS
+SELECT * FROM Album;
+
+CREATE OR REPLACE VIEW Songs_View AS
+SELECT * FROM Songs;
+
+CREATE OR REPLACE VIEW Playlists_View AS
+SELECT * FROM Playlists;
+
+CREATE OR REPLACE VIEW Downloaded_Songs_View AS
+SELECT * FROM Downloaded_Songs;
+
+CREATE OR REPLACE VIEW History_View AS
+SELECT * FROM History;
+/*
+--display views
+select * from Most_Listened_Song_Of_The_Month;
+select * from Most_Downloaded_Song_And_Album;
+select * from Most_Used_Promocodes;
+select * from Most_Listened_Genre_By_Customer;
+select * from Most_Used_Payment_Type;
+select * from Avg_Transaction_Price_Per_Customer;
+select * from Song_Count_By_Genre;
+select * from Revenue_Per_Transaction;
+select * from Most_Active_Customers;
+select * from Revenue_By_Payment_Type;
+select * from Transactions_Per_Customer_Per_Month;
+select * from Avg_Promocode_Discount;
+select * from Most_Popular_Genre_Active_Subscribers;
+select * from Total_Revenue_Per_Customer;
+select * from Avg_Downloads_Per_Song;
+SELECT * FROM Customers_View;
+SELECT * FROM Promocodes_View;
+SELECT * FROM Payment_Method_View;
+SELECT * FROM Bank_Account_View;
+SELECT * FROM Card_View;
+SELECT * FROM Transaction_View;
+SELECT * FROM Album_View;
+SELECT * FROM Songs_View;
+SELECT * FROM Playlists_View;
+SELECT * FROM Downloaded_Songs_View;
+SELECT * FROM History_View;
+*/
+
+/*
+SELECT * FROM Customers;
+
+SELECT * FROM Promocodes;
+
+SELECT * FROM Payment_Method;
+
+SELECT * FROM Bank_Account;
+
+SELECT * FROM Card;
+
+SELECT * FROM Transaction;
+
+SELECT * FROM Album;
+
+SELECT * FROM Songs;
+
+SELECT * FROM Playlists;
+
+SELECT * FROM Downloaded_Songs;
+
+SELECT * FROM History;
+*/
