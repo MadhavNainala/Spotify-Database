8e91f0129997f306cf3a3a8357c99d10c5d61bff
Srikanth Chilaka <113320805+SrikanthChilaka@users.noreply.github.com>
2023-12-01 22:07:49 -0500

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,4 +1,74 @@
---views 
+--Views
+CREATE OR REPLACE VIEW Most_Downloaded_Songs AS
+SELECT s.Song_Id, s.Song_Name, a.Album_Name, COUNT(*) AS Download_Count
+FROM Songs s
+JOIN Downloaded_Songs ds ON s.Song_Id = ds.Song_Id
+JOIN Album a ON s.Album_Id = a.Album_Id
+GROUP BY s.Song_Id, s.Song_Name, a.Album_Name
+ORDER BY Download_Count DESC;
+/
+
+
+SELECT * FROM Most_Downloaded_Songs;
+
+CREATE OR REPLACE VIEW Most_Listened_Song_And_Album AS
+SELECT s.Song_Id, s.Song_Name, a.Album_Name, COUNT(*) AS Listen_Count
+FROM Songs s
+JOIN History h ON s.Song_Id = h.Song_Id
+JOIN Album a ON s.Album_Id = a.Album_Id
+GROUP BY s.Song_Id, s.Song_Name, a.Album_Name
+ORDER BY Listen_Count DESC;
+/
+
+
+SELECT * FROM Most_Listened_Song_And_Album;
+
+
+CREATE OR REPLACE VIEW Most_Used_Promocodes AS
+SELECT p.Promocode_Id, p.Promocode_Name, COUNT(*) AS Usage_Count
+FROM Promocodes p
+JOIN Transaction t ON p.Promocode_Id = t.Promocode_Id
+GROUP BY p.Promocode_Id, p.Promocode_Name
+ORDER BY Usage_Count DESC;
+/
+
+SELECT * FROM Most_Used_Promocodes;
+
+CREATE OR REPLACE VIEW Transaction_Counts_By_Payment_Type AS
+SELECT 
+    (SELECT COUNT(*) FROM Transaction t
+     JOIN Payment_Method pm ON t.Customer_Id = pm.Customer_Id
+     WHERE pm.Payment_Type = 'Bank_Account') AS Bank_Account_Transactions,
+
+    (SELECT COUNT(*) FROM Transaction t
+     JOIN Payment_Method pm ON t.Customer_Id = pm.Customer_Id
+     WHERE pm.Payment_Type = 'Card') AS Card_Transactions
+FROM dual;
+/
+
+SELECT * FROM Transaction_Counts_By_Payment_Type;
+
+
+CREATE OR REPLACE VIEW Transaction_Outcomes AS
+SELECT 
+    COUNT(CASE WHEN (t.Price + NVL(p.Discount, 0)) = 20 THEN 1 END) AS Succeeded_Transactions,
+    COUNT(CASE WHEN (t.Price + NVL(p.Discount, 0)) != 20 THEN 1 END) AS Failed_Transactions
+FROM Transaction t
+LEFT JOIN Promocodes p ON t.Promocode_Id = p.Promocode_Id;
+/
+
+SELECT * FROM Transaction_Outcomes;
+
+CREATE OR REPLACE VIEW Subscription_Status_Count AS
+SELECT 
+    COUNT(CASE WHEN Subscription = 'Y' THEN 1 END) AS Customers_With_Subscription,
+    COUNT(CASE WHEN Subscription = 'N' THEN 1 END) AS Customers_Without_Subscription
+FROM Customers;
+/
+
+SELECT * FROM Subscription_Status_Count;
+/
+
 CREATE OR REPLACE VIEW Most_Listened_Song_Of_The_Month AS
 SELECT s.Song_Id, s.Song_Name, COUNT(*) AS Listen_Count
 FROM Songs s
@@ -10,32 +80,7 @@ ORDER BY Listen_Count DESC
 FETCH FIRST 5 ROW ONLY;
 /
 
-CREATE OR REPLACE VIEW Most_Downloaded_Song_And_Album AS
-SELECT s.Song_Name, a.Album_Name
-FROM Songs s
-JOIN Album a ON s.Album_Id = a.Album_Id
-JOIN Downloaded_Songs ds ON s.Song_Id = ds.Song_Id
-GROUP BY s.Song_Name, a.Album_Name
-ORDER BY COUNT(*) DESC
-FETCH FIRST 5 ROW ONLY;
-/
-
-CREATE OR REPLACE VIEW Most_Used_Promocodes AS
-SELECT p.Promocode_Name, COUNT(*) AS Usage_Count
-FROM Promocodes p
-JOIN Transaction t ON p.Promocode_Id = t.Promocode_Id
-GROUP BY p.Promocode_Name
-ORDER BY Usage_Count DESC
-FETCH FIRST 5 ROW ONLY;
-/
-
-CREATE OR REPLACE VIEW Most_Listened_Genre_By_Customer AS
-SELECT s.Genre, COUNT(*) AS Listen_Count
-FROM History h
-JOIN Songs s ON h.Song_Id = s.Song_Id
-GROUP BY s.Genre
-ORDER BY Listen_Count DESC;
-/
+SELECT * FROM Most_Listened_Song_Of_The_Month;
 
 CREATE OR REPLACE VIEW Most_Used_Payment_Type AS
 SELECT pm.Payment_Type, COUNT(*) AS Usage_Count
@@ -44,11 +89,7 @@ GROUP BY pm.Payment_Type
 ORDER BY Usage_Count DESC;
 /
 
-CREATE OR REPLACE VIEW Avg_Transaction_Price_Per_Customer AS
-SELECT Customer_Id, AVG(Price) AS Avg_Price
-FROM Transaction
-GROUP BY Customer_Id;
-/
+SELECT * FROM Most_Used_Payment_Type;
 
 CREATE OR REPLACE VIEW Song_Count_By_Genre AS
 SELECT Genre, COUNT(*) AS Song_Count
@@ -56,19 +97,7 @@ FROM Songs
 GROUP BY Genre;
 /
 
-CREATE OR REPLACE VIEW Revenue_Per_Transaction AS
-SELECT Transaction_Id, SUM(Price) AS Total_Revenue, AVG(Price) AS Avg_Price
-FROM Transaction
-GROUP BY Transaction_Id;
-/
-
-CREATE OR REPLACE VIEW Most_Active_Customers AS
-SELECT Customer_Id, COUNT(*) AS Transaction_Count
-FROM Transaction
-GROUP BY Customer_Id
-ORDER BY Transaction_Count DESC
-FETCH FIRST 5 ROW ONLY;
-/
+SELECT * FROM Song_Count_By_Genre;
 
 CREATE OR REPLACE VIEW Revenue_By_Payment_Type AS
 SELECT pm.Payment_Type, SUM(t.Price) AS Total_Revenue
@@ -77,67 +106,7 @@ JOIN Transaction t ON pm.Customer_Id = t.Customer_Id
 GROUP BY pm.Payment_Type;
 /
 
-CREATE OR REPLACE VIEW Transactions_Per_Customer_Per_Month AS
-SELECT
-    Customer_Id,
-    TO_CHAR(Transaction_Time, 'YYYY-MM') AS Month,
-    COUNT(*) AS Transaction_Count
-FROM Transaction
-GROUP BY Customer_Id, TO_CHAR(Transaction_Time, 'YYYY-MM');
-/
-
-CREATE OR REPLACE VIEW Avg_Promocode_Discount AS
-SELECT
-    Promocode_Id,
-    AVG(Discount) AS Avg_Discount
-FROM Promocodes
-GROUP BY Promocode_Id;
-/
-
-CREATE OR REPLACE VIEW Most_Popular_Genre_Active_Subscribers AS
-SELECT
-    s.Genre,
-    COUNT(*) AS Subscriber_Count
-FROM
-    History h
-JOIN
-    Songs s ON h.Song_Id = s.Song_Id
-JOIN
-    Customers c ON h.Customer_Id = c.Customer_Id
-WHERE
-    c.Subscription = 'Y'
-GROUP BY
-    s.Genre
-ORDER BY
-    Subscriber_Count DESC;
-/
-
-CREATE OR REPLACE VIEW Total_Revenue_Per_Customer AS
-SELECT
-    Customer_Id,
-    SUM(Price) AS Total_Revenue
-FROM
-    Transaction
-GROUP BY
-    Customer_Id;
-/
-
-CREATE OR REPLACE VIEW Avg_Downloads_Per_Song AS
-SELECT
-    Song_Id,
-    AVG(Download_Count) AS Avg_Downloads
-FROM (
-    SELECT
-        Song_Id,
-        COUNT(*) AS Download_Count
-    FROM
-        Downloaded_Songs
-    GROUP BY
-        Song_Id
-) ds
-GROUP BY
-    Song_Id;
-/
+SELECT * FROM Revenue_By_Payment_Type;
 
 CREATE OR REPLACE VIEW Customers_View AS
 SELECT * FROM Customers;
@@ -171,23 +140,8 @@ SELECT * FROM Downloaded_Songs;
 
 CREATE OR REPLACE VIEW History_View AS
 SELECT * FROM History;
+
 /*
---display views
-select * from Most_Listened_Song_Of_The_Month;
-select * from Most_Downloaded_Song_And_Album;
-select * from Most_Used_Promocodes;
-select * from Most_Listened_Genre_By_Customer;
-select * from Most_Used_Payment_Type;
-select * from Avg_Transaction_Price_Per_Customer;
-select * from Song_Count_By_Genre;
-select * from Revenue_Per_Transaction;
-select * from Most_Active_Customers;
-select * from Revenue_By_Payment_Type;
-select * from Transactions_Per_Customer_Per_Month;
-select * from Avg_Promocode_Discount;
-select * from Most_Popular_Genre_Active_Subscribers;
-select * from Total_Revenue_Per_Customer;
-select * from Avg_Downloads_Per_Song;
 SELECT * FROM Customers_View;
 SELECT * FROM Promocodes_View;
 SELECT * FROM Payment_Method_View;
@@ -199,28 +153,4 @@ SELECT * FROM Songs_View;
 SELECT * FROM Playlists_View;
 SELECT * FROM Downloaded_Songs_View;
 SELECT * FROM History_View;
-*/
-
-/*
-SELECT * FROM Customers;
-
-SELECT * FROM Promocodes;
-
-SELECT * FROM Payment_Method;
-
-SELECT * FROM Bank_Account;
-
-SELECT * FROM Card;
-
-SELECT * FROM Transaction;
-
-SELECT * FROM Album;
-
-SELECT * FROM Songs;
-
-SELECT * FROM Playlists;
-
-SELECT * FROM Downloaded_Songs;
-
-SELECT * FROM History;
 */
